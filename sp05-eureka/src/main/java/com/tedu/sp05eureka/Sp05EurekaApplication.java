70336ae895b5f8ddb7e968386f091e59d2091d59
PublicCasual <1587779096@qq.com>
2019-10-09 10:02:07 +0800

    eureka注册中心

---
+++ sp05-eureka/src/main/java/com/tedu/sp05eureka/Sp05EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.tedu.sp05eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Sp05EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp05EurekaApplication.class, args);
+    }
+
+}
